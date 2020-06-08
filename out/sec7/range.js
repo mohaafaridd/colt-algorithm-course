5575f0bebc217055a92c0e6bcf428bdc0cb9c1ec
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-06-08 21:08:02 +0200

    moved algorithms to dedicated folder

--- out/sec7/range.js
+++
@@ -1,6 +0,0 @@
-var recursiveRange = function (number) {
-    return number === 0 ? 0 : (number += recursiveRange(number - 1));
-};
-console.log("recursiveRange(6)", recursiveRange(6));
-console.log("recursiveRange(10)", recursiveRange(10));
-//# sourceMappingURL=range.js.map
--- out/sec7/second.js
+++
@@ -1,3 +0,0 @@
-var sum = function (num) { return (num === 1 ? 1 : num + sum(num - 1)); };
-console.log("sum(10)", sum(10));
-//# sourceMappingURL=second.js.map
