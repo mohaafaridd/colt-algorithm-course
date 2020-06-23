3ed6e51a9478cefcd6f7d17db9c8f8f086f5d943
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-06-23 14:55:07 +0200

    push and pop

--- datastructure/doublyLinkedList.ts
+++ datastructure/doublyLinkedList.ts
@@ -12,4 +12,48 @@ export class DoublyLinkedLis<T> {
   public get length(): number {
     return this._length
   }
+
+  public push(value: T) {
+    const node = new Node(value)
+
+    if (!this._head) {
+      this._head = node
+    } else {
+      this._tail.next = node
+      node.prev = this._tail
+    }
+
+    this._length++
+    this._tail = node
+
+    return this
+  }
+
+  public pop() {
+    if (!this._head) return undefined
+
+    const tail = this._tail
+
+    if (this._length === 1) {
+      this._head = null
+      this._tail = null
+    } else {
+      const newTail = this._tail.prev
+      this._tail.prev = null
+      this._tail = newTail
+      this._tail.next = null
+    }
+
+    this._length--
+    return tail
+  }
 }
+
+const list = new DoublyLinkedLis<number>()
+
+list.push(1)
+list.push(2)
+list.push(3)
+list.pop()
+
+console.log(list)
