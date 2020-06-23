aee6730df3670fc1bda2ad70321191a6403c3795
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-06-23 15:16:35 +0200

    shift and unshift

--- datastructure/doublyLinkedList.ts
+++ datastructure/doublyLinkedList.ts
@@ -47,6 +47,39 @@ export class DoublyLinkedLis<T> {
     this._length--
     return tail
   }
+
+  public shift() {
+    if (!this._head) return undefined
+    const oldHead = this._head
+
+    if (this.length === 1) {
+      this._head = null
+      this._tail = null
+    } else {
+      this._head = oldHead.next
+      this._head.prev = null
+      oldHead.next = null
+    }
+
+    this._length--
+    return oldHead
+  }
+
+  public unshift(value: T) {
+    const node = new Node(value)
+
+    if (!this._head) {
+      this._head = node
+      this._tail = node
+    } else {
+      this._head.prev = node
+      node.next = this._head
+      this._head = node
+    }
+
+    this._length++
+    return this
+  }
 }
 
 const list = new DoublyLinkedLis<number>()
@@ -54,6 +87,6 @@ const list = new DoublyLinkedLis<number>()
 list.push(1)
 list.push(2)
 list.push(3)
-list.pop()
-
+list.shift()
+list.unshift(5)
 console.log(list)
